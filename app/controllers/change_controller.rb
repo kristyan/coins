b978a5a02b438e2bc48a2126306e60f4e12d21d0
Kristyan <kristyan@joltonline.com>
2011-01-13 18:57:18 +0000

    Added README and some additionalcomments

--- app/controllers/change_controller.rb
+++ app/controllers/change_controller.rb
@@ -1,11 +1,14 @@
 class ChangeController < ApplicationController
+  
   def index
   end  
+  
   def result
      @coins = params[:id].to_i
      begin
        @change = ChangeCalculator.calc_min_change(@coins).sort   
      rescue ArgumentError
+       # just return an empty result a bad param is given
        @change = ChangeCalculator::NO_CHANGE.sort
      end     
   end
--- app/models/change_calculator.rb
+++ app/models/change_calculator.rb
@@ -1,5 +1,7 @@
 module ChangeCalculator 
+  
    
+   # empty change set
    NO_CHANGE = {
      50 => 0,
      20 => 0,
@@ -8,7 +10,21 @@ module ChangeCalculator
      2 => 0,
      1 => 0
    }
-        
+    
+   # returns a hash containing the optimal change denomination for a given change value from 1 to 99 cents.
+   # This method assumes change can only consist of 1,2,5,10,20 and 50 cent coins, and generates the change set
+   # such that the amount of coins is minimized.
+   #
+   # For example 98 cents would return the following change hash (ie. 1*50c + 2*20c + 1*5c + 1*2c + 1*1c  )
+   # 
+   # {
+   #   50 => 1,
+   #   20 => 2,
+   #   10 => 0,
+   #   5 => 1,
+   #   2 => 1,
+   #   1 => 1
+   #  }       
    def self.calc_min_change(change, change_map = NO_CHANGE.dup )
       
        raise ArgumentError, "Invalid change amount passed: #{change}" if change < 0 || change > 99
